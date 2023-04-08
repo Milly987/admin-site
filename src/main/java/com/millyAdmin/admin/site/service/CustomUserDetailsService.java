7062cc3d33335acbc768819bec99c821d1a05b8d
milkahcode <kibanyumilkah@gmail.com>
2023-04-08 08:43:30 -0700

    authentication with spring boot

---
+++ src/main/java/com/millyAdmin/admin/site/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.millyAdmin.admin.site.service;
+
+
+import com.millyAdmin.admin.site.model.CustomUserDetails;
+import com.millyAdmin.admin.site.model.User;
+import com.millyAdmin.admin.site.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.security.core.userdetails.UserDetails;
+        import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
---
+++ src/test/java/com/millyAdmin/admin/site/AdminSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millyAdmin.admin.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
